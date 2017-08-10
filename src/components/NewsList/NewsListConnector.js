7b97b3f57932779a12456b6eae00da3abb306e46
abody <adrien.body-prestataire@laposte.fr>
2017-08-10 17:53:13 +0200

    news added

--- src/components/NewsList/NewsListConnector.js
+++ src/components/NewsList/NewsListConnector.js
@@ -15,9 +15,20 @@ const newsListQuery = gql`
    }
  `;
 
-const newsSubscription = gql`
+const newsAddedSubscription = gql`
   subscription {
     newsAdded {
+      id
+      title
+      url
+      votes
+    }
+  }
+`;
+
+const newsVotedRequest = gql`
+  subscription {
+    newsVoted {
       id
       title
       url
@@ -34,20 +45,38 @@ const NewsListConnector = graphql(newsListQuery, {
     props: props => {
         return {
             subscribeToNewNews: params => {
-                return props.data.subscribeToMore({
-                    document: newsSubscription,
-                    updateQuery: (prev, {subscriptionData}) => {
-                        if (!subscriptionData.data) {
-                            return prev;
+                return [
+                    props.data.subscribeToMore({
+                        document: newsAddedSubscription,
+                        updateQuery: (prev, {subscriptionData}) => {
+                            if (!subscriptionData.data) {
+                                return prev;
+                            }
+                            const newNews = subscriptionData.data.newsAdded;
+                            let news = [newNews, ...prev.news];
+                            news.sort(compareNombres);
+                            return Object.assign({}, prev, {
+                                news: news
+                            });
                         }
-                        const newNews = subscriptionData.data.newsAdded;
-                        let news = [newNews, ...prev.news];
-                        news.sort(compareNombres);
-                        return Object.assign({}, prev, {
-                            news: news
-                        });
-                    }
-                });
+                    }),
+                    props.data.subscribeToMore({
+                        document: newsVotedRequest,
+                        updateQuery: (prev, {subscriptionData}) => {
+                            if (!subscriptionData.data) {
+                                return prev;
+                            }
+                            const newEditedId = subscriptionData.data.newsVoted;
+                            let news = [...prev.news];
+                            let index = news.findIndex(x => x.id === newEditedId.id);
+                            news[index] = newEditedId;
+                            news.sort(compareNombres);
+                            return Object.assign({}, prev, {
+                                news: news
+                            });
+                        }
+                    })
+                ]
             },
             data: props.data,
             compareNombres: compareNombres
